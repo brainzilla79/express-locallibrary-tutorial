6fa51de47f1f1d3279729f61b066f6763dd5218c
Alex <alexanderhapp@me.com>
2018-03-11 17:31:35 -0700

    Add index function to book controller

--- models/author.js
+++ models/author.js
@@ -3,10 +3,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var AuthorSchema = new Schema({
-  first_name: {type: String, required: true, max: 100},
-  family_name: {type: String, required: true, max: 100},
-  date_of_birth: {type: Date},
-  date_of_death: {type: Date},
+  first_name: { type: String, required: true, max: 100 },
+  family_name: { type: String, required: true, max: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date }
 });
 
 AuthorSchema.virtual('name').get(function() {
