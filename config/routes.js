7ccfb0ac2075369872f65f63ec8175b3f4c3db10
Alex <alexanderhapp@me.com>
2018-03-18 17:57:06 -0700

    Add route middleware

--- config/routes.js
+++ config/routes.js
@@ -4,7 +4,7 @@ const genre_controller = require('../controllers/genreController');
 const book_instance_controller = require('../controllers/bookinstanceController');
 const user_controller = require('../controllers/userController');
 
-function loggedIn(req, res, next) {
+function protectedRoute(req, res, next) {
   if (req.user) {
     next();
   } else {
@@ -12,21 +12,44 @@ function loggedIn(req, res, next) {
   }
 }
 
+function authRoute(req, res, next) {
+  if (req.user) {
+    res.redirect('/');
+  } else {
+    next();
+  }
+}
+
 module.exports = function(app, passport) {
+
+  app.get('/', function(req, res) {
+    if (req.user) {
+      res.redirect('/catalog');
+    } else {
+      res.redirect('/login');
+    }
+  });
+
   /// SESSION ROUTES ///
 
-  app.get('/signup', user_controller.user_create_get);
-  app.post('/signup', passport.authenticate('local-signup', {
-    successRedirect: '/catalog',
-    failureRedirect: '/signup',
-    failureFlash: true,
-  }));
-  app.get('/login', user_controller.user_login_get);
-  app.post('/login',passport.authenticate('local-login', {
-    successRedirect: '/catalog',
-    failureRedirect: 'login',
-    failureFlash: true,
-  }));
+  app.get('/signup', authRoute, user_controller.user_create_get);
+  app.post(
+    '/signup',
+    passport.authenticate('local-signup', {
+      successRedirect: '/catalog',
+      failureRedirect: '/signup',
+      failureFlash: true
+    })
+  );
+  app.get('/login', authRoute, user_controller.user_login_get);
+  app.post(
+    '/login',
+    passport.authenticate('local-login', {
+      successRedirect: '/catalog',
+      failureRedirect: 'login',
+      failureFlash: true
+    })
+  );
   app.get('/logout', function(req, res) {
     req.logout();
     res.redirect('/login');
@@ -35,45 +58,153 @@ module.exports = function(app, passport) {
   /// CATALOG ROUTES ///
 
   /// BOOK ROUTES ///
-  app.get('/catalog', book_controller.index);
-  app.get('/catalog/book/create', book_controller.book_create_get);
-  app.post('/catalog/book/create', book_controller.book_create_post);
-  app.get('/catalog/book/:id/delete', book_controller.book_delete_get);
-  app.post('/catalog/book/:id/delete', book_controller.book_delete_post);
-  app.get('/catalog/book/:id/update', book_controller.book_update_get);
-  app.post('/catalog/book/:id/update', book_controller.book_update_post);
-  app.get('/catalog/book/:id', book_controller.book_detail);
-  app.get('/catalog/books', book_controller.book_list);
+  app.get('/catalog', protectedRoute, book_controller.index);
+  app.get(
+    '/catalog/book/create',
+    protectedRoute,
+    book_controller.book_create_get
+  );
+  app.post(
+    '/catalog/book/create',
+    protectedRoute,
+    book_controller.book_create_post
+  );
+  app.get(
+    '/catalog/book/:id/delete',
+    protectedRoute,
+    book_controller.book_delete_get
+  );
+  app.post(
+    '/catalog/book/:id/delete',
+    protectedRoute,
+    book_controller.book_delete_post
+  );
+  app.get(
+    '/catalog/book/:id/update',
+    protectedRoute,
+    book_controller.book_update_get
+  );
+  app.post(
+    '/catalog/book/:id/update',
+    protectedRoute,
+    book_controller.book_update_post
+  );
+  app.get('/catalog/book/:id', protectedRoute, book_controller.book_detail);
+  app.get('/catalog/books', protectedRoute, book_controller.book_list);
 
   /// AUTHOR ROUTES ///
-  app.get('/catalog/author/create', author_controller.author_create_get);
-  app.post('/catalog/author/create', author_controller.author_create_post);
-  app.get('/catalog/author/:id/delete', author_controller.author_delete_get);
-  app.post('/catalog/author/:id/delete', author_controller.author_delete_post);
-  app.get('/catalog/author/:id/update', author_controller.author_update_get);
-  app.post('/catalog/author/:id/update', author_controller.author_update_post);
-  app.get('/catalog/author/:id', author_controller.author_detail);
-  app.get('/catalog/authors', author_controller.author_list);
+  app.get(
+    '/catalog/author/create',
+    protectedRoute,
+    author_controller.author_create_get
+  );
+  app.post(
+    '/catalog/author/create',
+    protectedRoute,
+    author_controller.author_create_post
+  );
+  app.get(
+    '/catalog/author/:id/delete',
+    protectedRoute,
+    author_controller.author_delete_get
+  );
+  app.post(
+    '/catalog/author/:id/delete',
+    protectedRoute,
+    author_controller.author_delete_post
+  );
+  app.get(
+    '/catalog/author/:id/update',
+    protectedRoute,
+    author_controller.author_update_get
+  );
+  app.post(
+    '/catalog/author/:id/update',
+    protectedRoute,
+    author_controller.author_update_post
+  );
+  app.get(
+    '/catalog/author/:id',
+    protectedRoute,
+    author_controller.author_detail
+  );
+  app.get('/catalog/authors', protectedRoute, author_controller.author_list);
 
   /// GENRE ROUTES ///
-  app.get('/catalog/genre/create', genre_controller.genre_create_get);
-  app.post('/catalog/genre/create', genre_controller.genre_create_post);
-  app.get('/catalog/genre/:id/delete', genre_controller.genre_delete_get);
-  app.post('/catalog/genre/:id/delete', genre_controller.genre_delete_post);
-  app.get('/catalog/genre/:id/update', genre_controller.genre_update_get);
-  app.post('/catalog/genre/:id/update', genre_controller.genre_update_post);
-  app.get('/catalog/genre/:id', genre_controller.genre_detail);
-  app.get('/catalog/genres', genre_controller.genre_list);
+  app.get(
+    '/catalog/genre/create',
+    protectedRoute,
+    genre_controller.genre_create_get
+  );
+  app.post(
+    '/catalog/genre/create',
+    protectedRoute,
+    genre_controller.genre_create_post
+  );
+  app.get(
+    '/catalog/genre/:id/delete',
+    protectedRoute,
+    genre_controller.genre_delete_get
+  );
+  app.post(
+    '/catalog/genre/:id/delete',
+    protectedRoute,
+    genre_controller.genre_delete_post
+  );
+  app.get(
+    '/catalog/genre/:id/update',
+    protectedRoute,
+    genre_controller.genre_update_get
+  );
+  app.post(
+    '/catalog/genre/:id/update',
+    protectedRoute,
+    genre_controller.genre_update_post
+  );
+  app.get('/catalog/genre/:id', protectedRoute, genre_controller.genre_detail);
+  app.get('/catalog/genres', protectedRoute, genre_controller.genre_list);
 
   /// BOOKINSTANCE ROUTES ///
-  app.get('/catalog/bookinstance/create', book_instance_controller.bookinstance_create_get);
-  app.post('/catalog/bookinstance/create', book_instance_controller.bookinstance_create_post);
-  app.get('/catalog/bookinstance/:id/delete', book_instance_controller.bookinstance_delete_get);
-  app.post('/catalog/bookinstance/:id/delete', book_instance_controller.bookinstance_delete_post);
-  app.get('/catalog/bookinstance/:id/update', book_instance_controller.bookinstance_update_get);
-  app.post('/catalog/bookinstance/:id/update', book_instance_controller.bookinstance_update_post);
-  app.get('/catalog/bookinstance/:id', book_instance_controller.bookinstance_detail);
-  app.get('/catalog/bookinstances', book_instance_controller.bookinstance_list);
+  app.get(
+    '/catalog/bookinstance/create',
+    protectedRoute,
+    book_instance_controller.bookinstance_create_get
+  );
+  app.post(
+    '/catalog/bookinstance/create',
+    protectedRoute,
+    book_instance_controller.bookinstance_create_post
+  );
+  app.get(
+    '/catalog/bookinstance/:id/delete',
+    protectedRoute,
+    book_instance_controller.bookinstance_delete_get
+  );
+  app.post(
+    '/catalog/bookinstance/:id/delete',
+    protectedRoute,
+    book_instance_controller.bookinstance_delete_post
+  );
+  app.get(
+    '/catalog/bookinstance/:id/update',
+    protectedRoute,
+    book_instance_controller.bookinstance_update_get
+  );
+  app.post(
+    '/catalog/bookinstance/:id/update',
+    protectedRoute,
+    book_instance_controller.bookinstance_update_post
+  );
+  app.get(
+    '/catalog/bookinstance/:id',
+    protectedRoute,
+    book_instance_controller.bookinstance_detail
+  );
+  app.get(
+    '/catalog/bookinstances',
+    protectedRoute,
+    book_instance_controller.bookinstance_list
+  );
 
   app.use(function(req, res, next) {
     var err = new Error('Not Found');
@@ -91,6 +222,4 @@ module.exports = function(app, passport) {
     res.status(err.status || 500);
     res.render('error');
   });
-
-  
 };
